4510cd409513b8cd8ae155897ebed8e072579ef0
Jochen Stierberger <jochen@stierberger.com>
2023-12-16 15:59:24 -0600

    Add dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { redirect } from 'next/navigation'
+
+export default async function Dashboard() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const signOut = async () => {
+    'use server'
+
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+    await supabase.auth.signOut()
+    return redirect('/')
+  }
+
+  return <>
+    <form action={signOut}>
+      <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        Logout
+      </button>
+    </form>
+
+  </>
+}
