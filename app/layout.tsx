430234e330751c464025d8cab4ad57963aef4516
Jochen Stierberger <jochen@stierberger.com>
2023-12-17 09:10:45 -0600

    Remove background and handle via globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+      <body>
+        <main className="min-h-screen flex flex-col">
           {children}
         </main>
       </body>
